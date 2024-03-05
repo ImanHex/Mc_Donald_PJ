7deeb3c55d5e0dcef83a754b7d05707ea80b081f
0ctopusHead <yanwarut_s@cmu.ac.th>
2024-03-05 21:38:41 +0700

    experiment_1(SVC model)

--- Code/Model/experiment_1.py
+++ Code/Model/experiment_1.py
@@ -1,6 +1,12 @@
 import pickle
 from pathlib import Path
 import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
 
 
 stored_folder = Path(os.path.abspath('')).parent.parent / "data" / "processed" / "cleaned_df.pkl"
@@ -9,4 +15,39 @@ cleaned_data = pickle.load(input_file)
 
 
 if __name__ == "__main__":
-    print(cleaned_data)
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(cleaned_data['processed_review'])
+    y = cleaned_data['sentiment']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True, random_state=42)
+
+    rf1 = SVC()
+    rf1.fit(X_train, y_train)
+    pred_rf1 = rf1.predict(X_test)
+    print(f'Accuracy:  {accuracy_score(y_test,pred_rf1)}')
+
+    cm = confusion_matrix(y_test, pred_rf1)
+    print('Confusion Matrix:')
+    print(cm)
+
+    labels = ['Positive', 'Negative', 'Neutral']
+    cm = confusion_matrix(y_test, pred_rf1, labels=labels)
+
+    # Plotting the confusion matrix
+    fig, ax = plt.subplots()
+    cax = ax.matshow(cm)
+    plt.title('McDonald`s Sentiment Confusion Matrix')
+    fig.colorbar(cax)
+    ax.set_xticks(np.arange(len(labels)))
+    ax.set_yticks(np.arange(len(labels)))
+    ax.set_xticklabels(labels)
+    ax.set_yticklabels(labels)
+
+    # Add text annotations
+    for i in range(len(labels)):
+        for j in range(len(labels)):
+            plt.text(j, i, str(cm[i, j]), ha='center', va='center', color='black')
+
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.show()
+
